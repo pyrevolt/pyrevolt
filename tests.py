3826947739f11746c506efba71108fc40ddaadde
Fabio Almeida <me@fabioalmeida.dev>
2022-03-19 02:55:45 +0000

    Add Gateway class and added typing to variables

--- tests.py
+++ tests.py
@@ -1,28 +1,50 @@
+import asyncio
 import unittest
 import os
 import pyrevolt
 
 class HTTPTests(unittest.IsolatedAsyncioTestCase):
-    def setUp(self) -> None:
-        return super().setUp()
+    async def asyncTearDown(self) -> None:
+        await asyncio.sleep(0)
+        return await super().asyncTearDown()
 
     async def test_request(self) -> None:
-        client = pyrevolt.HTTPClient()
-        request = pyrevolt.Request(pyrevolt.Method.GET, "/")
+        client: pyrevolt.HTTPClient = pyrevolt.HTTPClient()
+        request: pyrevolt.Request = pyrevolt.Request(pyrevolt.Method.GET, "/")
         
-        result = await client.request(request)
+        result: dict = await client.Request(request)
         self.assertEqual(result["ws"], "wss://ws.revolt.chat")
-        await client.close()
+        await client.Close()
 
     async def test_fetch_user(self) -> None:
-        client = pyrevolt.HTTPClient()
-        user = "01FYEQ9FTJ62N39TGZ9P7BMCZD"
-        request = pyrevolt.Request(pyrevolt.Method.GET, "/users/" + user)
+        client: pyrevolt.HTTPClient = pyrevolt.HTTPClient()
+        user: str = "01FYEQ9FTJ62N39TGZ9P7BMCZD"
+        request: pyrevolt.Request = pyrevolt.Request(pyrevolt.Method.GET, "/users/" + user)
         request.AddAuthentication(os.getenv("token"))
         
-        result = await client.request(request)
+        result: dict = await client.Request(request)
         self.assertEqual(result["username"], "Fabio")
-        await client.close()
+        await client.Close()
+
+class GatewayTests(unittest.IsolatedAsyncioTestCase):
+    async def test_gateway_url(self) -> None:
+        gateway: pyrevolt.Gateway = pyrevolt.Gateway()
+        result: dict = await gateway.GetWebsocketURL()
+        self.assertEqual(result, "wss://ws.revolt.chat")
+        await gateway.Close()
+
+    async def test_gateway_connect(self) -> None:
+        self.gateway: pyrevolt.Gateway = pyrevolt.Gateway()
+        await self.gateway.Connect()
+        await self.gateway.Send({
+            "type": pyrevolt.GatewayEvent.Ping.value,
+            "data": 0
+        })
+        self.assertEqual({
+            "type": pyrevolt.GatewayEvent.Pong.value,
+            "data": 0
+        }, await self.gateway.Receive())
+        await self.gateway.Close()
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
