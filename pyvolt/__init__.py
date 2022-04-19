a803d302dd17b62ce0113810fd37a57addc792d7
Fabio Almeida <me@fabioalmeida.dev>
2022-04-19 01:07:03 +0100

    Added user and server structs to init

--- pyvolt/__init__.py
+++ pyvolt/__init__.py
@@ -3,5 +3,6 @@ from .gateway import Gateway, GatewayEvent
 from .events import *
 from .session import Session
 from .exceptions import ClosedSocketException
-from .structs.user import User
+from .structs.user import Relationship, Presence, Status, Bot, User
 from .structs.channels import Channel, SavedMessages, DirectMessage, Group, TextChannel, VoiceChannel
+from .structs.server import Category, SystemMessages, Role, Server
