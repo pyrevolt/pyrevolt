da68c92506e4901b2072ef0e1c5b22c28ce8eb88
Fabio Almeida <me@fabioalmeida.dev>
2022-07-05 22:47:41 +0100

    Added ServerChannel to init

--- pyrevolt/__init__.py
+++ pyrevolt/__init__.py
@@ -5,7 +5,7 @@ from .session import Session
 from .bot import Bot
 from .exceptions import ClosedSocketException
 from .structs.user import Relationship, Presence, Status, BotUser, User
-from .structs.channels import ChannelType, Channel, SavedMessages, DirectMessage, Group, TextChannel, VoiceChannel, Message, EmbedType, EmbedImageSize, Embed, Masquerade, Reply
+from .structs.channels import ChannelType, Channel, SavedMessages, DirectMessage, Group, ServerChannel, TextChannel, VoiceChannel, Message, EmbedType, EmbedImageSize, Embed, Masquerade, Reply
 from .structs.server import Category, SystemMessages, Role, Server
 from .structs.member import Member
 from .structs.invite import Invite, ServerInvite, GroupInvite
